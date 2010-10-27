8ebabbdcb50ae609ecf0bdedcd6f20076fa6f055
Philip Peitsch <philip.peitsch@gmail.com>
2010-10-28 08:19:45 +1100

    Added some more options to the benchmark tool to make it easier to profile

--- Transmute.Benchmark/Main.cs
+++ Transmute.Benchmark/Main.cs
@@ -8,28 +8,65 @@ namespace Transmute.Benchmark
     {
         public static void Main(string[] args)
         {
-            Console.Out.WriteLine("Native mapper conversion");
             var testFixture = new Benchmarks();
-            testFixture.SetUp();
-            var totalMs = testFixture.BenchmarkNative();
-            Console.Out.WriteLine("Total elapsed time: {0}ms  Total conversions: {1}  Conversions: {2}/s".With(
-                totalMs, Benchmarks.Total, 100 * Benchmarks.Total / totalMs));
+            double totalMs;
 
-            Console.Out.WriteLine("Transmute mapper conversion - Delegate");
-            for(int i = 0; i < 10; i++)
+            var initialised = false;
+            int total = 0;
+            for(int i = 0; i < args.Length; i++)
             {
+                if(int.TryParse(args[i], out total))
+                {
+                   initialised = true;
+                   break;
+                }
+            }
+            if(!initialised)
+                total = 10;
+
+            if(args.Length == 0)
+            {
+                Console.Out.WriteLine("Native mapper conversion");
                 testFixture.SetUp();
-                totalMs = testFixture.BenchmarkTransmute(MapBuilder.Delegate);
-                Console.Out.WriteLine("Total elapsed time: {0}ms  Total conversions: {1}  Conversions: {2}/s".With(totalMs, Benchmarks.Total, 100 * Benchmarks.Total / totalMs));
+                totalMs = testFixture.BenchmarkNative();
+                Console.Out.WriteLine("Total elapsed time: {0}ms  Total conversions: {1}  Conversions: {2}/s".With(
+                    totalMs, Benchmarks.Total, 100 * Benchmarks.Total / totalMs));
             }
 
-            Console.Out.WriteLine("Transmute mapper conversion - Emit");
-            for(int i = 0; i < 10; i++)
+            if(args.Length == 0 || args.Any(a => string.Equals(a, "delegate", StringComparison.CurrentCultureIgnoreCase)))
             {
-                testFixture.SetUp();
-                totalMs = testFixture.BenchmarkTransmute(MapBuilder.Emit);
-                Console.Out.WriteLine("Total elapsed time: {0}ms  Total conversions: {1}  Conversions: {2}/s".With(totalMs, Benchmarks.Total, 100 * Benchmarks.Total / totalMs));
+                Console.Out.WriteLine("Transmute mapper conversion - Delegate");
+                for(int i = 0; i < total; i++)
+                {
+                    testFixture.SetUp();
+                    totalMs = testFixture.BenchmarkTransmute(MapBuilder.Delegate);
+                    Console.Out.WriteLine("Total elapsed time: {0}ms  Total conversions: {1}  Conversions: {2}/s".With(totalMs, Benchmarks.Total, 100 * Benchmarks.Total / totalMs));
+                }
             }
+
+            if(args.Length == 0 || args.Any(a => string.Equals(a, "emit", StringComparison.CurrentCultureIgnoreCase)))
+            {
+                Console.Out.WriteLine("Transmute mapper conversion - Emit");
+                for(int i = 0; i < total; i++)
+                {
+                    testFixture.SetUp();
+                    totalMs = testFixture.BenchmarkTransmute(MapBuilder.Emit);
+                    Console.Out.WriteLine("Total elapsed time: {0}ms  Total conversions: {1}  Conversions: {2}/s".With(totalMs, Benchmarks.Total, 100 * Benchmarks.Total / totalMs));
+                }
+            }
+        }
+    }
+
+    public static class ArrayExtensions
+    {
+        public static bool Any(this string[] array, Func<string, bool> method)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(method(array[i]))
+                    return true;
+            }
+            return false;
         }
     }
 }
